f038e35f018320bb685d1d668bfb628137824049
Himanshu Nagrath <himanshu@Himanshus-MacBook-Air.local>
2020-05-14 01:28:19 +0530

    design improvements

--- src/App.js
+++ src/App.js
@@ -1,79 +1,111 @@
 import React from "react";
 
-function App() {
+const App = () => {
   return (
     <div
-      className="max-w-xl mx-auto h-screen rounded-lg shadow-lg px-32 pt-4 pb-64"
-      style={{ backgroundColor: "#efcc19" }}
+      className="h-screen max-w-lg pt-4 pb-64 mx-auto shadow-lg"
+      style={{ backgroundColor: "#efcc19", borderRadius: "20px" }}
     >
-      <div className="text-center text-2xl text-gray-700 mb-4">T E T R I S</div>
-      <div
-        className="w-full mx-auto mb-8 shadow-inner border border-gray-700"
-        style={{ height: "400px", backgroundColor: "#9ead86" }}
-      ></div>
-      <div className="flex -ml-8 mb-8">
-        <div className="flex-col">
-          <div
-            className="rounded-full w-10 h-10 border border-black ml-2 bg-green-500"
-            style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
-          ></div>
-          <span className="text-gray-700 text-sm">Pause(P)</span>
+      <div className="px-16">
+        <div className="mt-2">
+          <div className="flex items-center justify-between -mb-4">
+            <div className="-mb-4">
+              <div className="w-2 h-10 -mb-10 bg-black"></div>
+              <div className="w-24 h-2 ml-2 bg-black"></div>
+            </div>
+            <div className="flex flex-row-reverse -mb-4">
+              <div className="w-2 h-10 -mb-10 bg-black"></div>
+              <div className="w-24 h-2 bg-black"></div>
+            </div>
+          </div>
+          <div className="pt-1 -mb-6 text-2xl text-center text-gray-700">
+            T E T R I S
+          </div>
         </div>
-        <div className="ml-2">
+        <div className="px-8 mb-4 border-b-8 border-l-8 border-r-8 border-black">
           <div
-            className="rounded-full w-10 h-10 ml-2 border border-black bg-green-500"
-            style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
+            className="w-full mx-auto mt-8 mb-8 border border-gray-700 shadow-inner"
+            style={{ height: "400px", backgroundColor: "#9ead86" }}
           ></div>
-          <span className="text-gray-700 text-sm">Sound(S)</span>
-        </div>
-        <div className="ml-2">
-          <div
-            className="rounded-full w-10 h-10 ml-2 border border-black bg-red-700"
-            style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
-          ></div>
-          <span className="text-gray-700 text-sm ml-1">Reset(R)</span>
         </div>
       </div>
-      <div
-        className="w-20 h-20 rounded-full border border-black ml-56 -mt-16"
-        style={{
-          backgroundColor: "#5a65f1",
-          boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
-        }}
-      ></div>
-      <div className="flex items-center justify-between w-56 ml-40 -mt-4 pr-4 ">
+      <div className="px-12">
+        <div className="flex mb-8 -ml-4">
+          <div className="flex-col">
+            <div
+              className="w-12 h-12 ml-2 bg-green-500 border border-black rounded-full"
+              style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
+            ></div>
+            <span className="text-sm text-gray-700">Pause(P)</span>
+          </div>
+          <div className="ml-2">
+            <div
+              className="w-12 h-12 ml-2 bg-green-500 border border-black rounded-full"
+              style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
+            ></div>
+            <span className="text-sm text-gray-700">Sound(S)</span>
+          </div>
+          <div className="ml-2">
+            <div
+              className="w-12 h-12 ml-2 bg-red-700 border border-black rounded-full"
+              style={{ boxShadow: "inset 0 3px 6px hsla(0,0%,100%,.8)" }}
+            ></div>
+            <span className="ml-1 text-sm text-gray-700">Reset(R)</span>
+          </div>
+        </div>
+        <div className="flex ml-64 -mt-24">
+          <div
+            className="w-20 h-20 border border-black rounded-full"
+            style={{
+              backgroundColor: "#5a65f1",
+              boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
+            }}
+          ></div>
+          <span className="mt-8 ml-2 text-sm text-gray-700">Rotation</span>
+        </div>
+        <div className="flex items-center justify-between w-48 ml-48 ">
+          <div>
+            <div
+              className="w-20 h-20 -ml-2 border border-black rounded-full"
+              style={{
+                backgroundColor: "#5a65f1",
+                boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
+              }}
+            ></div>
+            <span className="ml-6 text-sm text-gray-700">Left</span>
+          </div>
+          <div>
+            <div
+              className="w-20 h-20 -mr-12 border border-black rounded-full"
+              style={{
+                backgroundColor: "#5a65f1",
+                boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
+              }}
+            ></div>
+            <span className="ml-6 text-sm text-gray-700">Right</span>
+          </div>
+        </div>
+        <div className="ml-64 -mt-6">
+          <div
+            className="w-20 h-20 border border-black rounded-full"
+            style={{
+              backgroundColor: "#5a65f1",
+              boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
+            }}
+          ></div>
+          <span className="ml-6 text-sm text-gray-700">Down</span>
+        </div>
         <div
-          className="w-20 h-20 rounded-full border border-black"
+          className="w-32 h-32 -mt-40 border border-black rounded-full"
           style={{
             backgroundColor: "#5a65f1",
             boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
           }}
         ></div>
-        <div
-          className="w-20 h-20 rounded-full border border-black"
-          style={{
-            backgroundColor: "#5a65f1",
-            boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
-          }}
-        ></div>
+        <span className="ml-4 text-sm text-gray-700">Drop(SPACE)</span>
       </div>
-      <div
-        className="w-20 h-20 rounded-full ml-56 -mt-4 border border-black"
-        style={{
-          backgroundColor: "#5a65f1",
-          boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
-        }}
-      ></div>
-      <div
-        className="w-32 h-32 rounded-full -mt-40 -ml-8 border border-black"
-        style={{
-          backgroundColor: "#5a65f1",
-          boxShadow: "inset 0 5px 10px hsla(0,0%,100%,.8)",
-        }}
-      ></div>
-      <span className="text-gray-700 text-sm -ml-2">Drop(SPACE)</span>
     </div>
   );
-}
+};
 
 export default App;
